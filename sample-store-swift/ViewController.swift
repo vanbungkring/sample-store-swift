b42106d3c1f16b7892732b96740df5d4082fb9d1
vanbungkring <prasetyo.mailbox@gmail.com>
2016-10-14 00:30:19 +0700

    Initial Commit

---
+++ sample-store-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample-store-swift
+//
+//  Created by Arie on 10/14/16.
+//  Copyright © 2016 Arie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
